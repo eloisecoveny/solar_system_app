f861696e2b26190b10120884cc1bc59342edca09
Eloise Coveny <eloisecoveny@gmail.com>
2019-03-02 12:01:23 +0000

    Set up App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,60 +1,34 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<template lang="html">
+
 </template>
 
 <script>
+import SolarSystem from './components/SolarSystem.vue'
+import PlanetDetail from './components/PlanetDetail.vue'
+import { eventBus } from './main.js'
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  name: "app",
+  data(){
+    return{
+      planets: [],
+      selectedPlanet: null
     }
+  },
+  components: {
+    SolarSystem,
+    PlanetDetail
+  },
+  mounted(){
+    fetch('http://localhost:3000/api/planets/')
+    .then(response => response.json())
+    .then(planets => this.planets = planets)
+  },
+  methods: {
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style lang="css" scoped>
 </style>
